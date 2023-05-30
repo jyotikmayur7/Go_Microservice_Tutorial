ac8ae98cca64bd16381cd7366d243e75220e6c57
Jyotik Mayur <heyjyotik@gmail.com>
2023-05-30 06:53:12 +0100

    Episode 1: Basic request and response handling

---
+++ working/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World")
+		d, error := io.ReadAll(r.Body)
+
+		if error != nil {
+			http.Error(rw, "Oops", http.StatusBadRequest)
+		}
+
+		fmt.Fprintf(rw, "Hello %s", d)
+	})
+
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goodbye World")
+	})
+
+	http.ListenAndServe(":9090", nil)
+}
