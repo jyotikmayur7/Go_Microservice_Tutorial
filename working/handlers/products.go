e812f74df9ec52b23961a66f2e04523ccfbf0648
Jyotik Mayur <heyjyotik@gmail.com>
2023-06-07 07:53:35 +0100

    Fetching id from request to perform update operation on it later in the PUT http request

--- working/handlers/products.go
+++ working/handlers/products.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 	"working/working/data"
 )
 
@@ -23,6 +25,30 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		p.addProduct(rw, r)
 		return
+	}
+
+	if r.Method == http.MethodPut {
+		// expect the id in URI
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 2 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+		}
+		p.l.Println("got id", id)
+
 	}
 	// catch all
 	rw.WriteHeader(http.StatusMethodNotAllowed)
